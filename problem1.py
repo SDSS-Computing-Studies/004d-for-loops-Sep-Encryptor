2ea1998a80ee1e0e3f559ebde14da574b8b017c3
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-02-28 19:13:24 -0800

    done

--- problem1.py
+++ problem1.py
@@ -22,3 +22,8 @@ enter a number:4
 ****
 
 """
+W=(input("enter width: ")).strip()
+W=int(W)
+for i in range(0,W):
+    print("*"*W,end="\n")
+    
--- problem2.py
+++ problem2.py
@@ -38,3 +38,13 @@ Enter a number: -4
 Invalid input
 """
 
+N=(input("input a number: ")).strip()
+N=int(N)
+C=N
+a=1
+for i in range(0,N):
+    a *= N
+    N -= 1
+a=str(a)
+C=str(C)
+print(C+"!"+" is "+a)
