4139609712a68ad995077df639249114a1df867d
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-02-28 19:30:44 -0800

    done

--- task2.py
+++ task2.py
@@ -22,4 +22,15 @@ Enter a name: Lebron
 That name is on the list
 """
 
-nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+N=(input("enter a name: ")).strip()
+NameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+a=0
+for i in range(5):
+    if NameList[i]==N:
+        print('That name is on the list')
+        a += 1
+        continue
+    if a==0:
+        print("That name is not on the list")
+        
+
